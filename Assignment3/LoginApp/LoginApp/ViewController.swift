64b780d2e1d14e816939300ea63bd0694a6ba778
Enoch Qu <enochqu@outlook.com>
2020-11-16 19:42:22 -0800

    assign 3

---
+++ Assignment3/LoginApp/LoginApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginApp
+//
+//  Created by Enoch Qu　 on 11/16/20.
+//  Copyright © 2020 Xin Enoch Qu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
