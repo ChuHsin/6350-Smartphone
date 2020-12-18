204a5d218e23a4c7792b6468cf707ad578e2cf7c
Enoch Qu <enochqu@outlook.com>
2020-12-17 22:29:21 -0800

    final project

---
+++ Final Project/UpgradedStockApp/UpgradedStockApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UpgradedStockApp
+//
+//  Created by Enoch Qu　 on 12/17/20.
+//  Copyright © 2020 Xin Enoch Qu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
